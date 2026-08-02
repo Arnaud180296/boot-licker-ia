d36b80b87742b5d60204d9247757235c361e0f03
Arnaud180296 <1802arkabimbingoy@gmail.com>
2026-08-02 14:17:52 +0200

    feat(db) : Mise en place des relations entre les modeles

---
+++ app/Models/Conversation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Conversation extends Model
+{
+    //
+    public function message():HasMany{
+        return $this->hasMany(Message::class);
+    }
+
+    //
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function llm():BelongsTo{
+        return $this->belongsTo(Llm::class);
+    }
+
+}
---
+++ app/Models/Llm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Llm extends Model
+{
+    //
+    public function conversation():HasMany{
+        return $this->hasMany(Conversation::class);
+    }
+}
